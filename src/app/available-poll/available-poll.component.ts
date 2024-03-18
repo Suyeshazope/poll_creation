b019dea8024bd20b1a9506827f9307c1ccdff6b1
Suyeshazope <96578526+Suyeshazope@users.noreply.github.com>
2024-03-18 10:58:26 +0530

    availablepoll changes

--- src/app/available-poll/available-poll.component.ts
+++ src/app/available-poll/available-poll.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PollService } from '../poll.service';
 import { CommonModule } from '@angular/common';
+import {MatCard , MatCardHeader , MatCardTitle , MatCardSubtitle , MatCardContent} from '@angular/material/card'
 
 @Component({
   selector: 'app-available-poll',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule , MatCard , MatCardHeader , MatCardTitle , MatCardSubtitle , MatCardContent],
   templateUrl: './available-poll.component.html',
   styleUrl: './available-poll.component.css'
 })
@@ -20,7 +21,9 @@ export class AvailablePollComponent implements OnInit {
   loadsavailablePolls() {
     this.pollService.availablePoll().subscribe(
       (data) => {
-        console.log(data);
+        // console.log(data);
+        // console.log(data[0].pollName);
+        this.availablePolls = data ;
       }
     )
   }
