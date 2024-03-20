6deaa134e412d35eb7f1f19d3255a0b3d126cf2f
Shivam Jagtap <104118674+Shivam-Jagtap@users.noreply.github.com>
2024-03-20 17:57:35 +0530

    available poll is perfect now,biroooo

--- src/app/available-poll/available-poll.component.ts
+++ src/app/available-poll/available-poll.component.ts
@@ -1,39 +1,117 @@
 import { Component, OnInit } from '@angular/core';
 import { PollService } from '../poll.service';
 import { CommonModule } from '@angular/common';
-import {MatCard , MatCardHeader , MatCardTitle , MatCardSubtitle , MatCardContent} from '@angular/material/card'
+import {
+  MatCard,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardSubtitle,
+  MatCardContent,
+} from '@angular/material/card';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-available-poll',
   standalone: true,
-  imports: [CommonModule , MatCard , MatCardHeader , MatCardTitle , MatCardSubtitle , MatCardContent],
+  imports: [
+    CommonModule,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+  ],
   templateUrl: './available-poll.component.html',
-  styleUrl: './available-poll.component.css'
+  styleUrl: './available-poll.component.css',
 })
 export class AvailablePollComponent implements OnInit {
+  availablePolls2: any[] = [];
+  votedPollsList: any[] = [];
   availablePolls: any[] = [];
+  votedPolls2: any[] = [];
 
-  constructor(private pollService: PollService , private router : Router) { }
+  constructor(
+    private pollService: PollService,
+    private router: Router,
+    private userService: UserService
+  ) {}
   ngOnInit(): void {
     this.loadsavailablePolls();
   }
 
   loadsavailablePolls() {
-    this.pollService.availablePoll().subscribe(
-      (data) => {
-        // console.log(data);
-        // console.log(data[0].pollName);
-        this.availablePolls = data ;
-        this.pollService.setPollName(this.availablePolls[0].pollName)
-      }
-    )
+    this.pollService.availablePoll().subscribe((data) => {
+      // console.log(data);
+      //  we will try to take voted polls by user
+      this.pollService
+        .getVotedPolls(this.userService.getUserName())
+        .subscribe((votedPolls) => {
+          this.votedPollsList = votedPolls;
+
+          this.availablePolls2 = data;
+
+          for (let i = 0; i < this.availablePolls2.length; i++) {
+            // iterate for each elemet in 2nd array
+            let flag = false;
+            for (let j = 0; j < this.votedPollsList.length; j++) {
+              if (
+                this.votedPollsList[j].pollId == this.availablePolls2[i].pollId
+              ) {
+                flag = true;
+                break;
+              }
+            }
+            if (!flag) {
+              this.availablePolls.push(this.availablePolls2[i]);
+            } else {
+              this.votedPolls2.push(this.availablePolls2[i]);
+            }
+          }
+          console.log(this.availablePolls);
+          console.log("voted poll ---------");
+          console.log(this.votedPolls2);
+          console.log("--------------");
+          // for (let i = 0; i < this.availablePolls.length; i++) {
+          //   console.log(this.availablePolls[i].creationTime.substring(11, 16));
+          //   // console.log(typeof(this.availablePolls[i].creationTime))
+          // }
+        });
+    });
   }
 
   onPollCardClicked(poll: any) {
     // Set pollName and pollId when a card is clicked
     this.pollService.setPollName(poll.pollName);
     this.pollService.setPollId(poll.pollId);
-    this.router.navigateByUrl("home/votingpoll");
+    this.router.navigateByUrl('home/votingpoll');
+  }
+
+  now = new Date();
+  remainingTime(time: any) {
+    let creationTime = time.substring(11, 16);
+    let hours = +time.substring(11, 13);
+    let min = +time.substring(14, 16);
+    // console.log(creationTime);
+    // console.log(hours);
+    // console.log(min);
+    // console.log('-----------------------');
+    let remHours =0
+    if(hours < 12){
+      remHours = hours + 23 - this.now.getHours() ;
+    }else{
+      remHours = hours + 23 - this.now.getHours() - 24;
+    }
+    
+    if (remHours < 0) remHours *= -1;
+    let remMin = min - this.now.getMinutes();
+    if (remMin < 0) remMin *= -1;
+    // console.log(remHours);
+    // console.log(remMin);
+    const obj = {
+      hours: remHours,
+      min: remMin,
+    };
+    return obj;
   }
 }
--- src/app/voting-poll/voting-poll.component.ts
+++ src/app/voting-poll/voting-poll.component.ts
@@ -3,28 +3,40 @@ import { PollService } from '../poll.service';
 import { UserService } from '../user.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {MatRadioButton , MatRadioGroup} from '@angular/material/radio'
-import { MatButtonModule} from '@angular/material/button'
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-voting-poll',
   standalone: true,
-  imports: [CommonModule , FormsModule , MatRadioButton , MatRadioGroup , MatButtonModule , MatCard],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatRadioButton,
+    MatRadioGroup,
+    MatButtonModule,
+    MatCard,
+  ],
   templateUrl: './voting-poll.component.html',
-  styleUrl: './voting-poll.component.css'
+  styleUrl: './voting-poll.component.css',
 })
 export class VotingPollComponent {
-  pollId = 0 ;
+  pollId = 0;
   options: any[] = [];
   selectedOptionId = 0;
-  msg = false ;
+  msg = false;
+  isVoted = true;
 
-  constructor(private pollService: PollService , private userService : UserService , private router : Router) { }
+  constructor(
+    private pollService: PollService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
-  pollName = this.pollService.getPollName() ;
-  userName = this.userService.getUserName() ;
+  pollName = this.pollService.getPollName();
+  userName = this.userService.getUserName();
   // pollId = this.pollService.getPollId() ;
 
   ngOnInit(): void {
@@ -33,24 +45,31 @@ export class VotingPollComponent {
 
   loadOptions() {
     console.log();
-    this.pollId = this.pollService.getPollId() ;
-    this.pollService.getOptions(this.pollId)
-      .subscribe(options => {
-        // console.log(options);
-        
-        this.options = options;
-      });
+    this.pollId = this.pollService.getPollId();
+    this.pollService.getOptions(this.pollId).subscribe((options) => {
+      // console.log(options);
+
+      this.options = options;
+    });
   }
 
   saveOption() {
     // Here, you can handle saving the selected options, such as sending them to a server or performing any other necessary action
-    console.log(this.selectedOptionId);
-    this.pollService.saveVote(this.pollId , this.selectedOptionId , this.userName).subscribe((data) => {
-      console.log(data);
-      this.msg = true;
-      setTimeout(() => {
-        this.router.navigateByUrl('home/polloption/available-polls')
-      }, 1500);
-    })
+    if (this.selectedOptionId == 0) {
+      this.isVoted = false;
+    } else {
+      this.isVoted = true;
+
+      console.log(this.selectedOptionId);
+      this.pollService
+        .saveVote(this.pollId, this.selectedOptionId, this.userName)
+        .subscribe((data) => {
+          console.log(data);
+          this.msg = true;
+          setTimeout(() => {
+            this.router.navigateByUrl('home/polloption/available-polls');
+          }, 1500);
+        });
+    }
   }
 }
